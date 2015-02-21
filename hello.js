fadfe6ef7d27a5445e273b524bc534b625580538
chrischris292 <cbchan2@illinois.edu>
2015-02-20 18:25:05 -0600

    stupid

--- hello.js
+++ hello.js
@@ -5,4 +5,3 @@ http.createServer(function (req, res) {
 }).listen(8080, '162.243.141.138');
 console.log('Server running at http://162.243.141.138:8080/');
 
-https://github.com/chrischris292/DigitalOcean.git

579b75d38983ab5086a393a9a764f10f4e65da6c
chrischris292 <cbchan2@illinois.edu>
2015-02-21 22:23:09 -0600

    updated

--- hello.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('lucas the gay\n');
-}).listen(8080, '162.243.141.138');
-console.log('Server running at http://162.243.141.138:8080/');
-
--- index.js
+++ index.js
@@ -1,34 +1,79 @@
-var page = require('webpage').create();
-var system = require('system');
-var fs = require('fs');
- 
-if(system.args.length !== 3) {
-  console.log('Usage: phantomjs text-scraper.js <url> <output file>');
-  phantom.exit();
-}
- 
-var url = system.args[1];
-var outfile = system.args[2];
- 
-page.onConsoleMessage = function(msg) {
-  console.log(msg);
-};
- 
-page.open(url, function(status) {
-  var output = url + '\n';
-  if(status === 'success') {
-    setTimeout(function() {
-      var text = page.evaluate(function () {
-        return document.title + '\n' + document.body.innerText;
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('../..')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port,"162.243.141.138", function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// Chatroom
+
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
       });
-      output += text;
-      fs.write(outfile, output);
-    }, 1000);
-    setTimeout(function () {
-      phantom.exit()
-    }, 1000);
-  } else {
-    console.log("Error!")
-    phantom.exit()
-  }
+    }
+  });
 });
